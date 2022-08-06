168d1d1ced54103047b884754ed083e35926d6eb
Luis Felipe <lfmarquesf@gmail.com>
2022-08-06 14:32:30 -0300

    ajusta jobs paralelos para 1

--- import_from_csv.rb
+++ import_from_csv.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
+require 'sidekiq'
+require './sidekiq/my-worker'
 require_relative './lib/import'
 require 'csv'
 
-rows = CSV.read('./data.csv', col_sep: ';')
-rows.shift
-import = Import.new('postgres', 'medical_records')
-import.drop_table
-import.create_table
-
-rows.each do |row|
-  import.insert_data(row)
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://myredis:6379/0' }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://myredis:6379/0' }
+end
+
+path = '/app/data.csv'
+MyWorker.perform_async(path)
